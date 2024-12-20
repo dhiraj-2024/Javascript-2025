32b4d75e4ea07d369d5535e9f6b915bfae5eee9f
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2024-12-20 13:14:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert("hello")// we are using node js not a browser
+
+let age = 22;
+let name = "dhiraj";
+let isLoggin = true;
+let state;
+
+
+// Number => 2^53
+// BigInt =>
+// string => ""
+// Boolean => true / false 
+// null => standalone value , we can assigin empty value
+// undefined => we are not assign values to the variables 
+// Symbol => uniqueness 
+
+
+//object
+
+// console.log(typeof null); object 
+// console.log(typeof undefined); undefined 
