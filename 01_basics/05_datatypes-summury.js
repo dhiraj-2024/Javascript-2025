752279c19a1ff0150d670c437884c600f95e8706
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2024-12-21 14:19:06 +0000

    done

--- 01_basics/05_datatypes-summury.js
+++ 01_basics/05_datatypes-summury.js
@@ -2,15 +2,30 @@
 
 // 1.Primitives: String, Number ,Boolean ,BigInt ,Symbol ,null ,undefined :
 
-let name = "dhiraj";
-let values = 45;
-let islogdin = true;
-// let bigvalue = 23435434536356345n
-let outsidetemp = null;
-let state;
-const id = Symbol("123");
+let name = "dhiraj"; //string
+let values = 45; //number
+let islogdin = true;  //boolean
+// let bigvalue = 23435434536356345n.  //bigint
+let outsidetemp = null; //object
+let state; //undefined
+const id = Symbol("123"); //symbole
 const newid = Symbol("123")
 
-// console.log(id === newid);
+console.log(id === newid);
 
 // 2.Non_Primitives: Array , Object , function :
+
+const student =["dhiraj","suyog","pooja","radha"];//object
+
+
+myobj ={
+    Myname:"dhiraj",
+    age : 21,
+    city: "nashik"
+}//object
+
+const myfirstfunction = function(){
+    console.log("hello js ");
+}//function
+
+console.log(typeof student)
