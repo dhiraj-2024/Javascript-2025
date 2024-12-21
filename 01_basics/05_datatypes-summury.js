9db8ed2fc28968ea215802c0df3fda4a1330bf22
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2024-12-21 14:09:34 +0000

    datatypes summury

---
+++ 01_basics/05_datatypes-summury.js
@@ -0,0 +1,16 @@
+// DATATYPES 
+
+// 1.Primitives: String, Number ,Boolean ,BigInt ,Symbol ,null ,undefined :
+
+let name = "dhiraj";
+let values = 45;
+let islogdin = true;
+// let bigvalue = 23435434536356345n
+let outsidetemp = null;
+let state;
+const id = Symbol("123");
+const newid = Symbol("123")
+
+// console.log(id === newid);
+
+// 2.Non_Primitives: Array , Object , function :
