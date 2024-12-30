ea4f1eef767e292894a273096333b56e25931b38
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2024-12-30 23:00:32 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,16 @@
+const name = "Dhiraj"
+const repocount = 4
+
+// console.log(name + " "+ "have" + " "+ repocount + " "+ "repocount");  // complex to handle
+
+// console.log(`${name} have ${repocount} repocount`); // simple and easy to handle
+
+const gameName = new String('heydhiraj') // string and String is different 
+
+// console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase(gameName));
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('d'));
