d38f64b4e64f595fa869748dd064bf7bf9fb7dc7
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2024-12-31 16:52:14 +0000

    dates

---
+++ 01_basics/08_datesin.js
@@ -0,0 +1,29 @@
+// console.log(Date()
+let mydate = new Date();
+// console.log(mydate)
+// console.log(mydate.getTime())
+// console.log(mydate.getFullYear())
+// console.log(mydate.getTimezoneOffset())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleTimeString())
+// console.log(mydate.toISOString())
+
+let newdate = new Date(2024,1,23)
+// console.log(newdate)
+// console.log(newdate.toDateString())
+
+let currenttime =  Date.now()
+// console.log(currenttime)
+// console.log(newdate.getTime())
+
+
+console.log(Math.round((Date.now()/1000))) // convert in seconds
+
+
+// newdate.toLocaleDateString('default'{
+
+//     weekday:"narrow"
+
+// })
