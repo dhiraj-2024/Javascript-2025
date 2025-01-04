1de18eb48a507e70e92cc15766d8b0a91d636a40
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-01-04 03:35:48 +0000

    object part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,78 @@
+// const tinderUser = Object() 
+const tinderUser = {}
+
+tinderUser.id = "235asd"
+tinderUser.name = "pratik"
+tinderUser.age = 22
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser) // { id: '235asd', name: 'pratik', age: 22, isLoggedIn: false }
+
+ const newuser = {
+    email : "some@gmail.com",
+    username : {
+        fullname : {
+            firstname : "pratik",
+            lastname : "patil"
+        }
+    }
+ }
+
+//  console.log(newuser.username.fullname.firstname) //pratik 
+//  console.log(newuser.lastname) // undefined
+
+const obj1 = { 1:"a", 2: "b"}
+const obj2 = { 3:"a", 4: "b"}
+
+// const obj3 ={obj1,obj2}
+// console.log(obj3) //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+
+
+// const obj3 = Object.assign(obj1, obj2)
+// console.log(obj3) // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+// const obj3 = Object.assign( {} , obj1, obj2)
+// console.log(obj3)
+
+// const obj3 = {...obj1,...obj2};
+// console.log(obj3) // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+const user = [
+
+    {
+        id : "1",
+        email : "sldjlfkj@gmail.com"
+    },
+    {
+        id : "2",
+        email : "jgvdflfkj@gmail.com"
+    },
+    {
+        id : "3",
+        email : "slgfytvlfkj@gmail.com"
+    }
+]
+
+// user[1]
+// console.log(user[1])
+
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser)) // [ 'id', 'name', 'age', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)) // [ '235asd', 'pratik', 22, false ]
+
+
+// console.log(Object.entries(tinderUser)) 
+
+/* 
+output is : 
+
+[
+    [ 'id', '235asd' ],
+    [ 'name', 'pratik' ],
+    [ 'age', 22 ],
+    [ 'isLoggedIn', false ]
+]
+    */
+
+console.log(tinderUser.hasOwnProperty("name")) //  true
