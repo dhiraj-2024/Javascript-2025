fd8c0417b65cfa5075f899720e48616aaa725393
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-01-06 14:39:11 +0000

    scopes cover

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -19,11 +19,36 @@ if(true){
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log("Inner A : ", a) //Inner A :  10 
+    // console.log("Inner A : ", a) //Inner A :  10 
 }
 
 // console.log(a)  // a is not defined
 // console.log(b)   // b is not defined
 // console.log(c) //30 
  
-console.log(a) //488
+// console.log(a) //488
+
+function one(){
+
+    const username="dhiraj"
+    function two(){
+        const website="youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+     two()
+}
+one()
+
+if(true){
+
+    const username ="dhiraj"
+if(username === "dhiraj"){
+    const website ="yt"
+    console.log(username + website)
+
+}
+// console.log(website)
+
+}
+// console.log(username)
