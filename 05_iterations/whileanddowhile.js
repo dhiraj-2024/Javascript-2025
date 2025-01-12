b695362bbdae8748478e531db8287f2adbfd2002
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-01-12 06:14:59 +0000

    while loop

---
+++ 05_iterations/whileanddowhile.js
@@ -0,0 +1,20 @@
+let i=1
+
+while (i<=10) {
+    // console.log(`value is ${i}`); // print upto 10;
+    i= i+1
+}
+
+myarr = ["dhiraj","suyog","pramod","karan"]
+let arr=0
+while(arr<= myarr.length){
+    console.log(myarr[arr])
+    arr= arr+1
+
+}
+
+let j=1
+do {
+    // console.log(j);
+    j= j+2
+} while (j<=5);
